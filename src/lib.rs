4c0bad0a94f0ce7ee8a688f47bb25cbd1417dd26
Sibz <12050358+Sibz@users.noreply.github.com>
2020-09-24 18:17:19 +1000

    docs: updated inline doc

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,31 @@ mod thread_safe_hash_map;
 
 pub use thread_safe_static::*;
 
-/// Helper to create a thread safe static value
+/// # Helper to create a thread safe static value
 ///
 /// Can be used in global scope in module scope
 /// Required lazy_static imported to scope used
 ///
-/// ** Usage
-/// ts_static!(STATIC_NAME, Type)
+/// # Usage
+/// `ts_static!(STATIC_NAME, Type)`
 ///
-/// ** Example
+/// # Example
 /// `ts_static!(MY_STATIC_NAME, i32)`
 ///
+/// # Notes
 /// Static is set to a `ThreadSafeStruct<T>` to use the value
 /// use the with function, to set the value use the `set` function
 ///
 /// You can access the mutex field directly `value` but the helpers should
 /// be suffice for most needs
 ///
-///  *** Set value
+///  ### Set value
 ///  `MY_STATIC_NAME.set(Some(1337));`
-/// *** Use value
+///
+///  ### Use value
 /// `MY_STATIC_NAME.with(|value| { *value += 1 }).expect(".with failed");`
-/// *** Clear value
+///
+///  ### Clear value
 /// `MY_STATIC_NAME.set(None);`
 #[macro_export]
 macro_rules! ts_static {
