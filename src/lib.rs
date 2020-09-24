4ac9ff5d7e1ab04c3a414f2f61cc20618da12545
Sibz <12050358+Sibz@users.noreply.github.com>
2020-09-25 07:44:47 +1000

    feat: add ts_static_with_default macro

--- src/lib.rs
+++ src/lib.rs
@@ -42,3 +42,57 @@ macro_rules! ts_static {
         }
     }
 }
+
+
+
+/*use lazy_static::lazy_static;
+fn default_value() -> i32 { 123 }
+
+ts_static_with_default!(MY_STATIC2, i32, default_value());
+
+fn tt() {
+    MY_STATIC2.with(|x| {
+        assert_eq!(*x, 123);
+    });
+}*/
+
+/// # Same as ts_static only can be assigned a value from a function
+/// Calue must be identifier of a function
+///
+///
+/// # Example
+///
+/// ```
+/// use lazy_static::lazy_static;
+/// use ts_static::*;
+///
+/// fn my_default() -> i32 { 123 }
+///
+/// ts_static_with_default!(MY_STATIC, i32, my_default);
+/// MY_STATIC.with(|x| {
+///         assert_eq!(123, *x);
+///     });
+/// ```
+#[macro_export]
+macro_rules! ts_static_with_default {
+    ($name:ident, $type_ty: ty,$default_function:ident) =>
+    {
+        lazy_static! {
+            static ref $name: ThreadSafeStruct<$type_ty> = ThreadSafeStruct {
+                value: std::sync::Mutex::new(Some($default_function())),
+            };
+        }
+    }
+}
+
+use lazy_static::lazy_static;
+fn do_something() -> i32 {
+    123
+}
+ts_static_with_default!(MY_STATIC, i32, do_something);
+#[test]
+fn test_ts_static_with_default() {
+    MY_STATIC.with(|x| {
+        assert_eq!(123, *x)
+    }).expect("Error");
+}
